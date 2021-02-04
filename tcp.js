aaa4d1d12b24bbf06477b4b428dad08db756dd49
AG1L1TYx7 <71088402+AG1L1TYx7@users.noreply.github.com>
2021-02-03 22:02:17 -0800

    Delete tcp.js

--- tcp.js
+++
@@ -1,9 +0,0 @@
-var net = require('net');
-
-//the handler arguement is automatically set as listener for the console
-var server = net.createServer(function (socket) {
-    console.log("Connection from" + socket.remoteAddress);
-    socket.end("hello world\n");
-});
-
-server.listen(7000, "127.0.0.1");
